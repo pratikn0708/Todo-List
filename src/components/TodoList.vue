f2bd0ab05b8ca2a3defcc13cd943b7258d2b6fa4
unknown <pratikgarg0708@gmail.com>
2020-06-23 22:54:26 +0530

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p class="display-3">Vue Crash Course</p>
+            </div>
+        </div>
+        <div class="row">
+            <p>Add Create Form</p>
+        </div>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <ul class="list-group">
+                    <Todo v-for="(todo, index) in todos" :key="index"
+                        :todoString="todo.todoString"
+                        :completed="todo.completed"
+                     />
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Todo from "./todo";
+export default {
+    components:{
+        Todo
+    },
+    data(){
+        return{
+            todos:[
+                {todoString: "Wake Early", completed:true},
+                {todoString: "Learn Cooking", completed:false},
+                {todoString: "Do Competitive Coding", completed:true},
+                {todoString: "Start Investment", completed:false},
+                {todoString: "Eat Healthy", completed:false},
+                {todoString: "Complete GoLang", completed:true}
+            ]
+        };
+    },
+    methods:{
+        addTodo(newTodo){
+            this.todos.push({
+                todoString: newTodo,
+                completed:false
+            });
+        },
+        toggleTodo(todo){
+            todo.completed=!todo.completed;
+        },
+        editTodo(todo,newTodoString){
+            todo.todoString=newTodoString;
+        },
+        deleteTodo(deleteTodo){
+            this.todos=this.todos.filter(todo => todo.todoString !== deleteTodo.todoString);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todo.vue
@@ -0,0 +1,32 @@
+<template>
+    <li class="d-flex align-items-center list-group-item">
+        <button class="btn border-0 text-left flex-grow-1">{{todoString}}</button>
+        <form class="flex-grow-1">
+            <input type="text" class="form-control" />
+        </form>
+        <button class="btn btn-outline-primary">Edit</button>
+        <button class="btn btn-outline-danger">Delete</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props:{
+        todoString: String,
+        completed: Boolean
+    },
+    data(){
+        return{
+            isEditing: false,
+            newTodoString: ""
+        }
+    }
+}
+</script>
+
+<style scoped>
+.completed{
+    text-decoration: line-through;
+}
+
+</style>
